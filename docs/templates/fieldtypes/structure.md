4340643338543798665f15fae5580f827f6e6c1d
Bryan Nielsen <bryan@packettide.com>
2025-01-09 16:59:18 -0500

    Improve template documentation, including examples in both Twig and Blade

--- docs/templates/fieldtypes/structure.md
+++
@@ -1,7 +0,0 @@
-# Structure
-
-The Structure fieldtype allows you to select an entry managed by Structure in the control panel and when used in a template it will output the URL to that entry.
-
-```twig
-{{ entry.structure }}
-```
---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+export default {
+  ...MDXComponents,
+  Tabs,
+  TabItem,
+};
